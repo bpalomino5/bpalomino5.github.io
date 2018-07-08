6aa37366c2c7ace125d09c015028de8d4ad6cace
Brandon Palomino <12bpalomino@gmail.com>
2018-07-08 16:25:45 -0700

    setup nav bar and about section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
+import linkedlogo from "./linkedin.png";
+import { Image } from "react-bootstrap";
+
+const NavMenu = () => {
+  return (
+    <div className="navMenu">
+      <div className="menuitemBox">
+        <a className="menuItem">Home</a>
+        <a className="menuItem">Education</a>
+        <a className="menuItem">Experience</a>
+        <a className="menuItem">Portfolio</a>
+      </div>
+      <a href="https://www.linkedin.com/in/brandon-palomino/">
+        <Image src={linkedlogo} width="40px" />
+      </a>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
@@ -7,8 +25,20 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Brandon Palomino</h1>
+          <NavMenu />
         </header>
-        <p className="App-intro">Welcome to my world!</p>
+        <div className="homeStyle">
+          <div className="aboutSection">
+            <h3 align="left">About</h3>
+            <p align="left">
+              <br />My Story
+              <br />
+              <br />
+              My family made a choice to migrate from Peru to California in
+              order to change their world and in consequence my world as well.
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
