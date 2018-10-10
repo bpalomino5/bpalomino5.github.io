1fe190351336d980b807c9be6d39fe34bf8e1540
Brandon Palomino <12bpalomino@gmail.com>
2018-10-09 20:01:40 -0700

    compressed images and testing browerrouter

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./App";
 import ScrollToTop from "./components/ScrollToTop";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <ScrollToTop>
       <App />
     </ScrollToTop>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 registerServiceWorker();
